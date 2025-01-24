7b5cb813ff4cd30a71240bf10a9c9264bc9981d8
Haranck <harankrishna03@gmail.com>
2025-01-24 18:17:23 +0530

    minor changes

--- controllers/admin/customerController.js
+++ controllers/admin/customerController.js
@@ -1,73 +1,80 @@
-const User = require("../../models/userSchema");
+const User = require("../../models/userSchema")
 
-const customerInfo = async (req, res) => {
-    try {
-        let search = "";
-        if (req.query.search) {
-            search = req.query.search; // Accessing search input from query
-        }
 
-        let page = 1;
-        if (req.query.page) {
-            page = parseInt(req.query.page); // Parse the page as an integer
-        }
 
-        const limit = 5;
-        const userData = await User.find({
-            isAdmin: false,
-            $or: [
-                { name: { $regex: ".*" + search + ".*", $options: "i" } },
-                { email: { $regex: ".*" + search + ".*", $options: "i" } }
-            ]
-        })
-            .limit(limit)
-            .skip((page - 1) * limit)
-            .exec();
+const customerInfo = async (req,res) =>{
+   try {
+      
+      let search = ""
+      if(req.query.search){
+         search = req.query.search; //backendil llath access  cheyth serchik vekknnu
 
-        const count = await User.countDocuments({
-            isAdmin: false,
-            $or: [
-                { name: { $regex: ".*" + search + ".*", $options: "i" } },
-                { email: { $regex: ".*" + search + ".*", $options: "i" } }
-            ]
-        });
+      }
+      let page = 1
+      if(req.query.page){
+         page = req.query.page
+      }
 
-        const totalPages = Math.ceil(count / limit);
+      const limit =5
+      const userData = await User.find({
+         isAdmin:false,
+         $or:[
 
-        res.render("customers", {
-            data: userData,
-            totalPages: totalPages,
-            currentPage: page,
-            search: search
-        });
-    } catch (error) {
-        console.error(error);
-        res.redirect("/pageerror");
-    }
-};
+            {name:{$regex:".*"+search+".*"}},
+            {email:{$regex:".*"+search+".*"}},
 
-const customerBlocked = async (req, res) => {
-    try {
-        let id = req.query.id;
-        await User.updateOne({ _id: id }, { $set: { isblocked: true } });
-        res.redirect("/admin/customers");
-    } catch (error) {
-        res.redirect("/pageerror");
-    }
-};
+         ]
+         
+      })
+      .limit(limit*1)
+      .skip((page-1)*limit)
+      .exec()   // chain of promise combine cheyunnu
 
-const customerunBlocked = async (req, res) => {
-    try {
-        let id = req.query.id;
-        await User.updateOne({ _id: id }, { $set: { isblocked: false } });
-        res.redirect("/admin/customers");
-    } catch (error) {
-        res.redirect("/pageerror");
-    }
-};
+      const count = await User.find({
+         isAdmin:false,
+         $or:[
 
-module.exports = {
-    customerInfo,
-    customerBlocked,
-    customerunBlocked
-};
+            {name:{$regex:".*"+search+".*"}},
+            {email:{$regex:".*"+search+".*"}},
+
+         ]
+      }).countDocuments()
+
+      res.render("customers", {
+         data: userData,
+         totalPages: totalPages,
+         currentPage: page,
+         search: search
+      });
+
+   } catch (error) {
+      console.error('Error in customerInfo:', error);
+      res.redirect('/pageerror');
+   }
+}
+
+const customerBlocked = async (req,res) =>{
+   try {
+      let id = req.query.id
+      await User.updateOne({_id:id},{$set:{isblocked:true}})
+      res.redirect('/admin/customers')
+   } catch (error) {
+      res.redirect('/pageerror')
+   }
+}
+
+const customerunBlocked =async (req,res) =>{
+   try {
+      let id =req.query.id
+      await User.updateOne({_id:id},{$set:{isblocked:false}})
+      res.redirect("/admin/customers")
+   } catch (error) {
+      res.redirect("/pageerror")
+   }
+}
+ 
+module.exports ={
+   customerInfo,
+   customerBlocked,
+   customerunBlocked
+}
